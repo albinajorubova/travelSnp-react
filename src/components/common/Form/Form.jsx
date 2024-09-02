7b43b0f8b9a2b95b584291b47852e14a199211cc
albina <dzorubovaa@gmail.com>
2024-09-02 15:09:03 +0300

    Add input.jsx, remove unused packeges, moved const DATA above form

--- src/components/common/Form/Form.jsx
+++ src/components/common/Form/Form.jsx
@@ -1,138 +1,64 @@
-import React, { useState } from 'react';
-import cx from 'classnames';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
-import { useForm, Controller } from 'react-hook-form';
-import InputMask from 'react-input-mask';
 import Link from 'next/link';
 
+import Input from './Input';
 import RadioBtn from './RadioBtn';
 
 import s from './Form.module.scss';
 
+const DATA = [
+  {
+    label: 'Имя',
+    type: 'text',
+    placeholder: 'Введите Ваше имя',
+    required: true,
+  },
+  {
+    label: 'Направление',
+    type: 'select',
+    options: [
+      { value: '', label: 'Куда хотите поехать', isPlaceholder: true },
+      { value: 'Турция', label: 'Турция' },
+      { value: 'Египет', label: 'Египет' },
+    ],
+    required: true,
+  },
+  {
+    label: 'Email',
+    type: 'email',
+    placeholder: 'example@mail.com',
+    required: true,
+  },
+  {
+    label: 'Телефон',
+    type: 'text',
+    placeholder: '+7 ( _ _ _ ) _ _ _ - _ _ - _ _',
+    required: true,
+    component: 'masked',
+  },
+  {
+    label: 'Дата от',
+    type: 'text',
+    placeholder: 'ДД.ММ.ГГГГ',
+    required: true,
+    onFocus: setInputType => setInputType('date'),
+    onBlur: setInputType => setInputType('text'),
+  },
+  {
+    label: 'Дата до',
+    type: 'text',
+    placeholder: 'ДД.ММ.ГГГГ',
+    required: true,
+    onFocus: setInputType => setInputType('date'),
+    onBlur: setInputType => setInputType('text'),
+  },
+];
+
 const Form = () => {
   const { register, handleSubmit, control, reset } = useForm();
   const onSubmit = data => console.log(data);
-
-  const DATA = [
-    {
-      label: 'Имя',
-      type: 'text',
-      placeholder: 'Введите Ваше имя',
-      required: true,
-    },
-    {
-      label: 'Направление',
-      type: 'select',
-      options: [
-        { value: '', label: 'Куда хотите поехать', isPlaceholder: true },
-        { value: 'Турция', label: 'Турция' },
-        { value: 'Египет', label: 'Египет' },
-      ],
-      required: true,
-    },
-    {
-      label: 'Email',
-      type: 'email',
-      placeholder: 'example@mail.com',
-      required: true,
-    },
-    {
-      label: 'Телефон',
-      type: 'text',
-      placeholder: '+7 ( _ _ _ ) _ _ _ - _ _ - _ _',
-      required: true,
-      component: 'masked',
-    },
-    {
-      label: 'Дата от',
-      type: 'text',
-      placeholder: 'ДД.ММ.ГГГГ',
-      required: true,
-      onFocus: setInputType => setInputType('date'),
-      onBlur: setInputType => setInputType('text'),
-    },
-    {
-      label: 'Дата до',
-      type: 'text',
-      placeholder: 'ДД.ММ.ГГГГ',
-      required: true,
-      onFocus: setInputType => setInputType('date'),
-      onBlur: setInputType => setInputType('text'),
-    },
-  ];
-
-  const Input = ({
-    label,
-    type,
-    placeholder,
-    onFocus,
-    onBlur,
-    register,
-    required,
-    component,
-    control,
-    options,
-  }) => {
-    const [inputType, setInputType] = useState(type);
-
-    if (component === 'masked') {
-      return (
-        <div className={s.labInp}>
-          <label className={s.label}>{label}</label>
-          <Controller
-            name="phone"
-            control={control}
-            rules={{ required }}
-            render={({ field }) => (
-              <InputMask
-                mask="+7 (999) 999-99-99"
-                placeholder="+7 (___) ___-__-__"
-                className={s.input}
-                {...field}
-              />
-            )}
-          />
-        </div>
-      );
-    }
-
-    if (type === 'select') {
-      return (
-        <div className={cx(s.labInp, s.arrow)}>
-          <label className={s.label}>{label}</label>
-          <select
-            name={label}
-            className={cx(s.input, s.select)}
-            {...register(label, { required })}
-          >
-            {options.map(option => (
-              <option
-                key={option.value}
-                value={option.value}
-                className={option.isPlaceholder ? s.hide : undefined}
-              >
-                {option.label}
-              </option>
-            ))}
-          </select>
-        </div>
-      );
-    }
-
-    return (
-      <div className={s.labInp}>
-        <label className={s.label}>{label}</label>
-        <input
-          type={inputType}
-          className={s.input}
-          placeholder={placeholder}
-          onFocus={() => onFocus && onFocus(setInputType)}
-          onBlur={() => onBlur && onBlur(setInputType)}
-          {...register(label, { required })}
-        />
-      </div>
-    );
-  };
 
   return (
     <form
@@ -201,7 +127,7 @@ const Form = () => {
           >
             Нажимая кнопку, я принимаю условия&nbsp;
             <span>
-              <Link href="">Лицензионного договора </Link>
+              <Link href="#createTour">Лицензионного договора </Link>
             </span>
           </label>
         </div>
@@ -210,12 +136,12 @@ const Form = () => {
             type="submit"
             value="Найти тур"
           />
-          <button
+          <input
             type="reset"
+            className={s.resetBtn}
             onClick={() => reset()}
-          >
-            Сбросить
-          </button>
+            value="Сбросить"
+          />
         </div>
       </div>
     </form>
--- src/components/common/Form/RadioBtn.jsx
+++ src/components/common/Form/RadioBtn.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import cx from 'classnames';
-
-import PropTypes from 'prop-types';
 
 import s from './Form.module.scss';
 
